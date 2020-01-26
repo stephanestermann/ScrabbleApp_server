3990ad378cd1083726b5fa3447d7bf29d26f496e
Stephan Estermann <estermann@gmail.com>
2020-01-26 15:27:53 +0100

    Teständerung

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,6 @@ const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
     
-    res.status(200).json({ message: 'Connected!' });
+    res.status(200).json({ message: 'I have changed the Output' });
 });
 module.exports = routes;
