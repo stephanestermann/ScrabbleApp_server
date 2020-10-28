a3e02cdeac422a939ec2c8762edd71f193c7c405
Stephan Estermann <estermann@gmail.com>
2020-10-28 07:22:02 +0100

    Update-test

--- routes/test.js
+++ routes/test.js
@@ -2,6 +2,6 @@ const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
     
-    res.status(200).json({ message: 'I have changed the Output' });
+    res.status(200).json({ message: 'Soon, I will save here the Scrabbledata :-)' });
 });
 module.exports = routes;
