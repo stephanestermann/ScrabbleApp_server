e73da6d5a16b7af934926ab145e8ac60a44c9c61
Stephan Estermann <estermann@gmail.com>
2021-01-29 16:57:15 +0100

    backup to googleDrive and env

--- server.js
+++ server.js
@@ -5,26 +5,21 @@ const express = require("express");
 const routes = require('./routes/resultRoutes');
 const bodyParser = require('body-parser');
 const logger = require('morgan');
-const path = require("path");
+const config = require('./config.js');
 
 /**
  * App Variables
  */
 const app = express();
-const PORT = process.env.PORT || "3000";
-const NODE_ENV = process.env.NODE_ENV || 'development';
-
 
 /**
  *  App Configuration
  */
-app.set('port', PORT);
-app.set('env', NODE_ENV);
 app.use(logger('tiny'));
 app.use(bodyParser.json());
 
 /**
- * Routes Definitions
+ * CORS-Config
  */
 app.options("/*", function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
@@ -33,20 +28,23 @@ app.options("/*", function(req, res, next){
   res.sendStatus(200);
 
 });
-
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
+
+/**
+ * Routes Definitions
+ */
 app.use('/', routes);
 
 /**
  * Server Activation
  */
-app.listen(PORT, () => {
+app.listen(config.PORT, config.HOST, () => {
     console.log(
-      `Express Server started on Port ${app.get('port')} | Environment : ${app.get('env')}`
+      `Express Server started on http://${config.HOST}:${config.PORT} | Environment : ${config.NODE_ENV}`
     );
 });
 module.exports = app;
